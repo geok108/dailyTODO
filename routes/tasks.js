a095c3f4b9580cec7bacf7dfa693ed07c95eaa67
george <geokleanthous94@gmail.com>
2018-01-29 02:13:37 +0000

    Initial commit

---
+++ routes/tasks.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://geok:1234@ds117158.mlab.com:17158/dailytodo', ['tasks']);
+
+//Get all tasks
+router.get('/tasks', function(req, res, next) {
+  db.tasks.find(function(err, tasks) {
+
+    if (err) {
+      res.send(err)
+
+    }
+    res.json(tasks);
+  });
+
+});
+
+//Get one task
+router.get('/task/:id', function(req, res, next) {
+  db.tasks.findOne({
+    _id: mongojs.ObjectId(req.params.id)
+  }, function(err, task) {
+
+    if (err) {
+      res.send(err)
+
+    }
+    res.json(task);
+  });
+
+});
+
+
+
+
+module.exports = router;
